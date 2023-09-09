00d5e8e7c3c5ea70d54aaaa20ac91bda5266d309
Divya Macharla <divmac17@gmail.com>
2023-09-09 14:23:47 +0530

    commit react sample repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const user = {
+  name : 'Divya Macharla',
+  url : 'https://www.google.com',
+};
+const listOfCourses = [
+  {id : 1,title : 'React for beginners from A to Z'},
+  {id : 2, title : 'Learn Angular for beginners'},
+  {id : 3, title : 'Learn Angular for Intermediate level'}
+];
+const courses = listOfCourses.map(c => 
+  <li key={c.id} style={{color : 'magenta'}}> 
+    <a href="{c.id}">
+      {c.title}
+    </a> 
+  </li>
+);
+export default function MyApp(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding:'8px'}}>
+      <h1 style={{textAlign:'center'}}>
+        Welcome to helloworld platform 
+      </h1>
+      <p><b>Instructor details</b></p>
+      <p>------------------------</p>
+      <p>
+        {user.name} 
+      </p>
+      <MyButton/>
+      <h2>Available Courses for FREE</h2>
+        <ul>{courses}</ul>
     </div>
   );
 }
 
-export default App;
+function MyButton(){
+  const [count,setCount] = useState(1000);
+  function follow(){
+    setCount(count + 1);
+  }
+  return(
+    <button onClick={follow} style={{backgroundColor:'#61dafb',border:'none'}}>
+      Follow on Linkedin ({count} followers)
+    </button>
+  );
+}
